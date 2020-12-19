631045b73fd1a45c2975241429824485de6671f3
kate <ketrina113@gmail.com>
2020-12-19 19:00:54 +0200

    auth added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,7 @@ Route::group(['prefix'=>'tasks'], function (){
     Route::get('/create', 'Tasks\TasksController@create')
         ->name('tasks.create');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
