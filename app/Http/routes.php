484a38a51c581823844e2b5aab638e4c9ec5ec71
kate <ketrina113@gmail.com>
2020-12-22 10:26:23 +0200

    routes for lang

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,24 +10,60 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/'. App\Http\Middleware\LocaleMiddleware::$mainLanguage);
 });
 
-Route::group(['prefix'=>'tasks'], function (){
-    Route::get('/', 'Tasks\TasksController@index')
-    ->name('tasks.index');
+Route::group(['prefix' => App\Http\Middleware\LocaleMiddleware::getLocale()], function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-    Route::get('/create', 'Tasks\TasksController@create')
-        ->name('tasks.create');
+    Route::group(['prefix'=>'tasks'], function (){
+        Route::get('/', 'Tasks\TasksController@index')
+            ->name('tasks.index');
 
-    Route::post('/', 'Tasks\TasksController@add')
-        ->name('tasks.add');
+        Route::get('/create', 'Tasks\TasksController@create')
+            ->name('tasks.create');
 
-    Route::delete('/{task}', 'Tasks\TasksController@delete')->name('tasks.delete');
+        Route::post('/', 'Tasks\TasksController@add')
+            ->name('tasks.add');
+
+        Route::delete('/{task}', 'Tasks\TasksController@delete')->name('tasks.delete');
+    });
+
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+
+
 });
+Route::get('setlocale/{lang}', function ($lang) {
 
-Route::auth();
+    $referer = Redirect::back()->getTargetUrl(); //URL предыдущей страницы
+    $parse_url = parse_url($referer, PHP_URL_PATH); //URI предыдущей страницы
 
-Route::get('/home', 'HomeController@index');
+    //разбиваем на массив по разделителю
+    $segments = explode('/', $parse_url);
+
+    //Если URL (где нажали на переключение языка) содержал корректную метку языка
+    if (in_array($segments[1], App\Http\Middleware\LocaleMiddleware::$languages)) {
+
+        unset($segments[1]); //удаляем метку
+    }
+
+    //Добавляем метку языка в URL (если выбран не язык по-умолчанию)
+    if ($lang != App\Http\Middleware\LocaleMiddleware::$mainLanguage){
+        array_splice($segments, 1, 0, $lang);
+    }
+
+    //формируем полный URL
+    $url = Request::root().implode("/", $segments);
+
+    //если были еще GET-параметры - добавляем их
+    if(parse_url($referer, PHP_URL_QUERY)){
+        $url = $url.'?'. parse_url($referer, PHP_URL_QUERY);
+    }
+    return redirect($url); //Перенаправляем назад на ту же страницу
+
+})->name('setlocale');
